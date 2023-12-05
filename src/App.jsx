50310f9b17655a25aa51d777d7218205a2ac1dda
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-12-05 20:13:34 +0000

    first commit and setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Title</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, non.</p>
+    </div>
+  )
+}
+
+export default App
