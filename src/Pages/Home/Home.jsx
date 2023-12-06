e537d20b6712a89b15e6958f14fe821165309f14
RickyAnchores <ricky23anchores@gmail.com>
2023-12-06 22:23:21 +0000

    edit home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-<div className='Home flex flex-wrap md:flex-col m-5 gap-4'>
-    <div className="user p-4 w-full md:h-full  md:w-2/4 rounded-2xl flex flex-col justify-center items-start">
+<div className='Home h-screen p-5 flex flex-wrap  md:flex-col gap-4'>
+    <div className="user p-16 w-full  md:h-full  md:w-2/4 rounded-2xl flex flex-col justify-center items-start">
         <img src="https://rickyanchores.netlify.app/static/media/RikoAI.42d1aba710ba4d61e093.jpeg" alt="userImg" />
         <h1 className='title'>Ricky A</h1>
         <p>is a Data technician based in London with expertise in Data Analysis, currently working as a</p>
         <h2>Frontend Developer</h2>
     </div>
-    <div className="right p-4 w-full md:h-full  md:w-2/4 rounded-2xl flex flex-col justify-center items-center">
+    <div className="right p-6 w-full md:h-full  md:w-2/4 rounded-2xl flex flex-col justify-center items-center">
         <h1 className='text-xl'>Have a project in mind?</h1>
         <button className='btn bg-orange-600 p-2 rounded-2xl mt-5'>ricky23anchores@gmail.com</button>
     </div>
